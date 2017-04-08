3dc4e95a80fa8f3e8fb17023551d0e34523aa0aa
jimmykkimani@gmail.com <jimmykkimani@gmail.com>
2017-04-08 20:48:13 +0300

    (Chore) Add authentication forms

---
+++ app/forms.py
@@ -0,0 +1,40 @@
+from flask_wtf import Form
+from wtforms.fields import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired,url,Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from wtforms.fields.html5 import URLField
+from models import User
+
+
+
+class SigninForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                           Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
+
+class SignupForm(Form):
+    firstname = StringField('Username', validators=[
+        DataRequired(), Length(3, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Usernames must have only letters, '
+                                          'numbers, dots or underscores')])
+    lastname = StringField('Username', validators=[
+        DataRequired(), Length(3, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Usernames must have only letters, '
+                                          'numbers, dots or underscores')]) 
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                           Email()])                                                                                   
+    password = PasswordField('Password', validators=[ DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, email_field):
+        if User.query.filter_by(email=email_field.data).first():
+            raise ValidationError('Email already registered.')
+
+
+    def validate_email(self, email_field):
+        if User.query.filter_by(email=email_field.data).first():
+            raise ValidationError('Email already registered.')
+
