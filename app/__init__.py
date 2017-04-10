f8e0f452eeb65565491f736ab38cc3976798122d
oscamaina <mainaoscar40@gmail.com>
2017-04-11 00:44:19 +0300

    (Fix) fixed bug

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'D[\xa5E\x89\x01\xf9'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'Andela-dlc.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'Andela-dlc.db')
 app.config['DEBUG'] = True
 db = SQLAlchemy(app)
 
@@ -19,5 +20,5 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
-import models
-import views
+from . import models
+from . import views
--- manage.py
+++ manage.py
@@ -1,20 +1,26 @@
 from app import app, db
-from app.models import User
+from flask_bootstrap import Bootstrap
+from app.models import User, Items
 from flask_script import Manager, prompt_bool
 
 manager = Manager(app)
+Bootstrap(app)
+
 
 @manager.command
 def initdb():
     db.create_all()
-    db.session.add(User(username='admin', email='admin@andela.com',password='andela'))
+    db.session.add(
+        User(username='admin', email='admin@andela.com', password='andela'))
     db.session.commit()
-    print ('Intialized')
+    print('Intialized')
+
 
 @manager.command
 def dropdb():
     db.drop_all()
-    print ('Done')
+    print('Done')
+
 
 if __name__ == '__main__':
     manager.run()
