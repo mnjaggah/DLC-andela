583bc432fd43926f3bb614e2523d6582f217b994
oscamaina <mainaoscar40@gmail.com>
2017-04-11 00:43:47 +0300

    (Feature) created course model

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ def load_user(userid):
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
@@ -35,3 +36,11 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return "<User '{}'>".format(self.username)
+
+
+class Items(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=False)
+    description = db.Column(db.String(200), unique=False)
+    #user_requests = db.relationship('User', foreign_keys=[user_id], backref='user')
