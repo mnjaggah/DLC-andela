52e010e522f0aef6d8d10a8e951c12741a7172e6
jimmykkimani@gmail.com <jimmykkimani@gmail.com>
2017-04-11 16:11:10 +0300

    (Chore) Add learner checkpoints views

--- app/views.py
+++ app/views.py
@@ -65,6 +65,13 @@ def signup():
 def user():
     # user = User.query.filter_by(username=username).first_or_404()
     return render_template('user_profile.html')
+@app.route('/user/my_checkpoints')    
+def user_checkpoints():
+    return render_template('my_checkpoint.html')
+
+@app.route('/user/my_checkpoints/tasks')    
+def user_user_tasks():
+    return render_template('user_tasks.html')  
 
 @app.errorhandler(404)
 def page_not_found(e):
