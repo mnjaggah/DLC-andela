520c6aa54e76da9f564794444b31249a4e4bf517
Gchesta <elimushwari@gmail.com>
2017-04-11 14:16:20 +0300

    (Fix) Fix import module error

--- app/track.py
+++ app/track.py
@@ -13,7 +13,7 @@ session = DBSession()
 class Challenge(Base):
     """The class Card will be used to create a table that
     will store the information regarding the Challenges"""
-    __tablename__ = "Challenge"
+    __tablename__ = "challenges"
     challenge_id = Column(Integer, primary_key=True, autoincrement=True)
     board_name = Column(String(50))
     skill_name = Column(String(50))
@@ -24,7 +24,7 @@ class Challenge(Base):
         self.board_name = board_name
         self.skill_name = skill_name
         self.challenge_name = challenge_name
-        self.status = "To-Do"
+        self.status = "Todo"
     
     def change_to_doing(self):
         self.status = "Doing"
@@ -38,20 +38,20 @@ class Challenge(Base):
 class Skill(Base):
     """The class Card will be used to create a table that
     will store the information regarding the Card"""
-    __tablename__ = "cards"
+    __tablename__ = "skills"
     board_name = Column(String(50))
-    name = Column(String(50), primary_key=True, autoincrement=False)
+    skill_name = Column(String(50), primary_key=True, autoincrement=False)
     status = Column(String(10))
     
-    def __init__(self, board_name, name):
+    def __init__(self, board_name, skill_name):
         self.board_name = board_name
-        self.name = name
-        self.status = "To-Do"
+        self.skill_name = skill_name
+        self.status = "Todo"
     
     def add_challenge(self, challenge_name):
         new_challenge = Challenge(self.board_name, self.skill_name, challenge_name)
         session.add(new_challenge)
-        session.comit()
+        session.commit()
         return new_challenge
     
     def change_to_doing(self):
@@ -70,13 +70,13 @@ class Board(Base):
     board_id = Column(Integer, primary_key=True, autoincrement=False)
     name = Column(String(50), unique=True)
     
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, board_name):
+        self.board_name = board_name
     
-    def add_skill(self, card_name):
-        new_card = Skill(self.name, card_name)
-        session.add(new_card)
-        session.comit()
+    def add_skill(self, skill_name):
+        new_skill = Skill(self.board_name, skill_name)
+        session.add(new_skill)
+        session.commit()
         return new_skill
 
 Base.metadata.create_all(engine)
