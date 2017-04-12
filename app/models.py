48668e9f53244778f8f29bf3380c2263a72124b3
Jomo <jomomg@gmail.com>
2017-04-12 10:01:46 +0300

    (chore) Add models for tracking courses

--- app/models.py
+++ app/models.py
@@ -16,7 +16,9 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True)
     password_hash = db.Column(db.String)
     is_admin = db.Column(db.Boolean, default=False)
-    is_learner = db.Column(db.Boolean, default=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    facilitator_id = db.Column(db.Integer, db.ForeignKey('facilitator.user_id'))
+    previous_courses = db.Column(db.PickleType)
 
     @property
     def password(self):
@@ -37,17 +39,11 @@ class User(db.Model, UserMixin):
         return "<User '{}'>".format(self.username)
 
 
-class Learner(User):
-    facilitator = db.Column(db.String(80))
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
-    previous_courses = db.Column(db.PickleType)
-
-
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     description = db.Column(db.Text)
-    learners = db.relationship('Learner', lazy='dynamic')
+    learners = db.relationship('User', lazy='dynamic')
     tasks = db.relationship('Task', backref='course', lazy='dynamic')
 
 
@@ -64,3 +60,10 @@ class Target(db.Model):
     name = db.Column(db.String)
     is_done = db.Column(db.Boolean)
     task_id = db.Column(db.String, db.ForeignKey('task.id'))
+
+
+class Facilitator(db.Model):
+    __tablename__ = 'facilitator'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    learners = db.relationship('User', foreign_keys=[User.facilitator_id], lazy='dynamic')
+
