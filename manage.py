25edddafc6fb5a1f093d8e038225b7427cda9b15
mnjaggah <maria.njaggah@gmail.com>
2017-04-13 00:25:25 +0300

    (Chore) Modify initialized admin user instance

--- manage.py
+++ manage.py
@@ -1,13 +1,21 @@
+from flask_bootstrap import Bootstrap
 from app import app, db
 from app.models import User
 from flask_script import Manager, prompt_bool
 
 manager = Manager(app)
+Bootstrap(app)
 
 @manager.command
 def initdb():
     db.create_all()
-    db.session.add(User(username='admin', email='admin@andela.com',password='andela'))
+    db.session.add(User(
+    	username='admin',
+    	email='admin@andela.com',
+    	password='andela',
+    	is_admin=True,
+    	is_learner=False
+    	))
     db.session.commit()
     print ('Intialized')
 
